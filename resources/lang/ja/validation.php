6b5bbbae5cb6df81e96381f0cc54f502940a9813
yu_hirasaka <yu.hirasaka03@gmail.com>
2026-08-03 22:30:36 +0900

    feat: バリデーションエラーメッセージの日本語化のため、ロケールを日本語に変更・validation.phpの追加

---
+++ resources/lang/ja/validation.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'required' => ':attributeは必須です。',
+    'string' => ':attributeは文字列で入力してください。',
+    'max' => [
+        'string' => ':attributeは:max文字以内で入力してください。',
+    ],
+    'attributes' => [
+        'title' => 'タイトル',
+        'description' => '説明',
+    ],
+];
